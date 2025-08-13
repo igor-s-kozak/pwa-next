ef4d3e1ae4a98c6d5a75726b82047ea1fcaca7b8
Igor Kozak <i.kozak@tg.dunice.net>
2025-08-13 18:04:21 +0300

    Fix infinite reload.Change next.config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 import withPWA from 'next-pwa'
 
-const nextConfig: NextConfig = withPWA(
+const nextConfigWithPWA = withPWA(
   {
     dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: false,
+    // register: true,
+    // skipWaiting: true,
+    // disable: false,
 
   });
 
-export default nextConfig;
+
+export default nextConfigWithPWA({})
