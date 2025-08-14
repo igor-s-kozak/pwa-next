2cd1f2a6bda90c040a5fe2e7bb6b38cd25ec2677
Igor Kozak <i.kozak@tg.dunice.net>
2025-08-14 12:08:30 +0300

    Add tests for Task component

--- __tests__/components/Task.test.tsx
+++ __tests__/components/Task.test.tsx
@@ -1,7 +1,8 @@
+import { act } from 'react';
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, createEvent, screen } from '@testing-library/react'
 import { Task } from "../../src/app/components/Task";
-import { addTodo, deleteTodo, getAllTodos } from '../../api';
+import { addTodo, getAllTodos } from '../../api';
 import { v4 as uuidv4 } from 'uuid'
 import { ITask } from '@/types/tasks';
 
@@ -16,62 +17,68 @@ jest.mock("next/navigation", () => (
             replace: jest.fn(),
             refresh: jest.fn(),
         }),
-
     }));
 
 jest.mock('../../api', () => ({
     getAllTodos: jest.fn(() => Promise.resolve(todos)),
-    addTodo: jest.fn((task) => todos.push(task)),
-    deleteTodo: jest.fn((id) => { todos = todos.filter(todo => todo.id !== id) })
+    addTodo: jest.fn((task) => Promise.resolve().then(() => todos.push(task))),
+    deleteTodo: jest.fn((id) => Promise.resolve().then(() => todos = todos.filter(todo => todo.id !== id))),
+    editTodo: jest.fn(task => Promise.resolve().then(() => {
+        todos = todos.map(todo => {
+            if (todo.id === task.id) {
+                return { ...todo, text: task.text }
+            }
+            return todo;
+        })
+    }))
 })
 );
 
-
-
 describe('Task testing', () => {
+    const TaskWrapped = () => (
+        <table><tbody><Task task={{ id, text }} /></tbody></table>
+    )
     it('Edit task input has default value of current todo', () => {
-        render(<table><tbody><Task task={{ id, text }} /></tbody></table>)
+        render(<TaskWrapped />)
         const input = screen.getByPlaceholderText('Your todo item');
         const value = input.getAttribute('value');
         expect(value).toEqual(text)
     })
 
-    // it('Delete button works', async () => {
-    //     await addTodo({ id, text })
-
-    //     render(<Task task={{ id, text }} />)
-    //     const deleteIcon = screen.getByText('Delete item');
-    //     const mouseEvent = new MouseEvent('click')
-    //     fireEvent(deleteIcon, mouseEvent);
-    //     const res = await getAllTodos()
-    //     console.log(res)
-    //     expect(res).not.toContainEqual({ id, text })
-    // })
-
-    it('Delete button works', async () => {
+    it('Delete button delete task', async () => {
         await addTodo({ id, text })
-        // await addTodo({id: id2, text})
-
-        render(<table><tbody><Task task={{ id, text }} /></tbody></table>)
+        render(<TaskWrapped />)
         const deleteIconTitle = screen.getByText('Delete item');
-        // deleteIcon.onclick = async () => {await deleteTodo(id); console.log('hello')};
-        const deleteIconSVGElement = deleteIconTitle.parentNode
-
-        const mouseEvent = new MouseEvent('click')
-
-        // deleteIcon.addEventListener('click', () => {deleteTodo(id); console.log('hi')})
-
-        // Array.from(deleteIcon.children).forEach(child => console.log(child.onClick))
-
-        fireEvent(deleteIconSVGElement!, mouseEvent);
+        const deleteIconSVGElement = deleteIconTitle.parentNode;
+        const mouseEvent = createEvent.click(deleteIconSVGElement!)
+        await fireEvent(deleteIconSVGElement!, mouseEvent);
         const res = await getAllTodos();
-
-        expect(res.length).toEqual(0);
-        // .toContainEqual({ id, text })
+        expect(res).not.toContainEqual({ id, text })
     })
 
+    it('Edit button click open modal', () => {
+        render(<TaskWrapped />)
+        const editIconTitle = screen.getByText('Edit item');
+        const editIconSVGElement = editIconTitle.parentNode;
+        const mouseEvent = createEvent.click(editIconSVGElement!);
+        fireEvent(editIconSVGElement!, mouseEvent);
+        const dialogEl = document.querySelector('dialog');
+        expect(dialogEl?.classList).toContain('modal-open');
+    })
 
-
+    it('Editing task', async () => {
+        await addTodo({ id, text })
+        render(<TaskWrapped />)
+        await act(() => {
+            const submitButton = screen.getByText('Submit');
+            const input = screen.getByPlaceholderText('Your todo item');
+            const mouseEvent = createEvent.click(submitButton);
+            fireEvent.change(input, { target: { value: 'New value' } })
+            fireEvent(submitButton, mouseEvent);
+        })
+        const res = await getAllTodos();
+        expect(res[0].text).toBe('New value');
+    })
 
 })
 
